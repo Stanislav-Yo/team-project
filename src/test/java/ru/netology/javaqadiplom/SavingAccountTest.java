99d0c65e728999d37ed49639cbddb8f0e5a65ad2
Vera Blok <xitraia@mail.ru>
2025-02-13 01:41:38 +0300

    added tests in SavingAccountTest

--- src/test/java/ru/netology/javaqadiplom/SavingAccountTest.java
+++ src/test/java/ru/netology/javaqadiplom/SavingAccountTest.java
@@ -6,6 +6,60 @@ import org.junit.jupiter.api.Test;
 public class SavingAccountTest {
 
     @Test
+    public void testExclusionRateIsNegative() {
+        SavingAccount account = new SavingAccount(
+                2_000,
+                1_000,
+                10_000,
+                -5
+        );
+
+        Assertions.assertThrows(IllegalArgumentException.class, () -> account.add(3_000));
+    }
+
+    @Test
+    public void testPayIfAmountIsNegative() {
+        SavingAccount account = new SavingAccount(
+                2_000,
+                1_000,
+                10_000,
+                5
+        );
+
+        boolean expected = false;
+        boolean actual = account.pay(-1_000);
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testPayRegular() {
+        SavingAccount account = new SavingAccount(
+                2_000,
+                1_000,
+                10_000,
+                5
+        );
+
+        boolean expected = true;
+        boolean actual = account.pay(500);
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testPayMoreThanLimit() {
+        SavingAccount account = new SavingAccount(
+                2_000,
+                1_000,
+                10_000,
+                5
+        );
+
+        boolean expected = false;
+        boolean actual = account.pay(1_500);
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
     public void shouldAddLessThanMaxBalance() {
         SavingAccount account = new SavingAccount(
                 2_000,
@@ -17,5 +71,29 @@ public class SavingAccountTest {
         account.add(3_000);
 
         Assertions.assertEquals(2_000 + 3_000, account.getBalance());
+    }
+
+    @Test
+    public void testYearChangeRegular() {
+        SavingAccount account = new SavingAccount(
+                2_000,
+                1_000,
+                10_000,
+                5
+        );
+
+        Assertions.assertEquals(2_000 * 5 / 100, account.yearChange());
+    }
+
+    @Test
+    public void testYearChangeBalanceMoreThanLimit() {
+        SavingAccount account = new SavingAccount(
+                11_000,
+                1_000,
+                10_000,
+                5
+        );
+
+        Assertions.assertEquals(11_000 * 5 / 100, account.yearChange());
     }
 }
